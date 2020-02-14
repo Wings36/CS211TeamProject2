d5ca8d04b575e37cc78823d6862cf3c4315fb3e3
Peter Hoang <peterhg7@gmail.com>
2020-02-14 14:24:19 -0800

    Project Assignments Released

--- src/AbstractList.java
+++ src/AbstractList.java
@@ -1,2 +1,28 @@
 public class AbstractList {
+    /*
+>bogdan
+size
+get
+toString
+
+>Logan
+indexOf
+isEmpty
+contains
+
+>Quynh
+add
+add(int, E)
+remove
+
+>Hon
+set
+clear
+checkIndex
+
+>Peter
+hasNext
+next
+remove
+     */
 }
