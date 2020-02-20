0258fb92fa6a1adc6944d8c3c7091c8056bfd2a6
honchen2000 <honchen2000@gmail.com>
2020-02-20 14:00:22 -0800

    I did some modify on checkIndex method.

--- src/AbstractList.java
+++ src/AbstractList.java
@@ -27,12 +27,5 @@ public abstract class AbstractList<E> implements List<E> {
 //clear
     public abstract void clear();
 //checkIndex
-    private void checkIndex(int index) {
-        if (index < 0 || index >= size()) {
-            throw new IndexOutOfBoundsException("index: " + index);
-        }
-    }
-
-
-
+    public abstract void checkIndeX(int index);
 }
