1c589c62189dd3c16a0204fe1d5d9c087489dbfd
Peter Hoang <peterhg7@gmail.com>
2020-02-20 13:45:34 -0800

    fixed part 1

--- src/AbstractList.java
+++ src/AbstractList.java
@@ -1,47 +1,35 @@
 import java.util.*;
 public abstract class AbstractList<E> implements List<E> {
-    private int size;
-    /*
->bogdan
-size
-get
-toString */
-    public int size() {
-        return size;
-    }
-    /*
->Logan
-indexOf
-isEmpty
-contains
-
->Quynh
-add
-add(int, E)
-remove
 
 
->Hon
-set
-clear
-checkIndex*/
+//>bogdan
+//size
+//get
+    public abstract E get(int index);
+//toString
+    public abstract int size();
+
+//>Logan
+//indexOf
+//isEmpty
+//contains
+
+//>Quynh
+//add
+//add(int, E)
+//remove
+
+
+//>Hon
+//set
+//clear
+//checkIndex*/
     private void checkIndex(int index) {
         if (index < 0 || index >= size()) {
             throw new IndexOutOfBoundsException("index: " + index);
         }
     }
-/*
 
 
-//    public boolean hasNext() {
-//        return position < size();
-//    }
-    public abstract String toString();
-//    public abstract boolean hasNext();
-//    public abstract E next();
-//
-//    public abstract void remove();
 
-
-    public abstract void add(int index, E value);
 }
